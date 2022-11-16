7852b4248381c13fea10076aba78f18f651a3db1
eCituk <ecituk@thefives.com.mx>
2022-11-16 08:45:31 -0500

    pruebas en test/unit/components con item.component.spec.ts

--- tests/unit/components/Item.component.spec.ts
+++ tests/unit/components/Item.component.spec.ts
@@ -17,7 +17,57 @@ describe('Item.component.vue', () => {
             }
         })
 
+        // Esto simplemente prueba que en alguna parte del texto
+        // renderizado por el componente aparece 'Unit test item 1',
+        // pero no es muy preciso.
         expect(wrapper.text()).to.include('Unit test item 1')
 
+        // Esto es más preciso, ya que estamos seleccionando el div con
+        // el nombre de la clase y comprobamos si muestra el texto correcto.
+        let domEl = wrapper.find('div.name')
+        expect(domEl.text()).to.equal('Unit test item 1')        
+
     })
+
+    it('has expected css class when selected is false', () => {
+        const model: ItemInterface = {
+            id: 2,
+            name: 'Unit test item 2',
+            selected: false
+        }
+
+        const wrapper = shallowMount(ItemComponent, {
+            props: {
+                model: model
+            }
+        })
+
+        // comprobar la lista de clases css del componente
+        const classes = wrapper.classes()
+
+        expect(classes).to.be.an('array').that.includes('item')
+        expect(classes).to.be.an('array').that.does.not.includes('selected')
+                        
+    })
+
+    it('has selected css class when selected is true', () => {
+        const model: ItemInterface = {
+            id: 3,
+            name: 'Unit test item 3',
+            selected: true /* cambiando a selected = true aquí */
+        }
+
+        const wrapper = shallowMount(ItemComponent, {
+            props: {
+                model: model
+            }
+        })
+
+        const classes = wrapper.classes()
+        expect(classes).to.be.an('array').that.includes('item')
+        expect(classes).to.be.an('array').that.includes('selected')
+                        
+    })
+
+    
 })
